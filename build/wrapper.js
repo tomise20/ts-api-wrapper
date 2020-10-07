ca40fe5bed0ed747dc7eb0c527183ca10b5107ac
tomise20 <vira.tamas97@gmail.com>
2020-10-07 08:23:57 +0200

    url and content-type handling

--- build/wrapper.js
+++ build/wrapper.js
@@ -4,18 +4,47 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
-http_1.default.get("http://localhost:3000/test", resp => {
-    let data = "";
-    //A chunk of data has been recived.
-    resp.on("data", chunk => {
-        data += chunk;
-    });
-    //the whole response has been recived. Print out the result.
-    resp.on("end", () => {
-        let url = JSON.parse(data).title;
-        console.log(url);
-    });
-})
-    .on("error", err => {
-    console.log("Error: " + err.message);
-});
+class Wrapper {
+    constructor() {
+        this.checkStatus = (statusCode) => {
+            if (statusCode == 403 || statusCode == 404 || statusCode == 500)
+                return false;
+            return true;
+        };
+        this.checkContentType = (headers) => {
+            if (headers === undefined)
+                return false;
+            let data = headers["content-type"].split(";");
+            if (data[0] == "application/json")
+                return true;
+            return false;
+        };
+    }
+    get(url) {
+        let request = http_1.default.get(url, resp => {
+            let data = "";
+            if (!this.checkStatus(resp.statusCode)) {
+                console.log(resp.statusMessage);
+                return resp.statusMessage;
+            }
+            resp.on("data", chunk => {
+                data += chunk;
+            });
+            resp.on("end", () => {
+                if (!this.checkContentType(resp.headers)) {
+                    console.log("Incorrect header!");
+                    return false;
+                }
+                let url = JSON.parse(data).title;
+                console.log(url);
+                return true;
+            });
+            return "success";
+        })
+            .on("error", err => {
+            console.log("Error: " + err.message);
+        });
+        request.end();
+    }
+}
+exports.default = Wrapper;
--- src/wrapper.ts
+++ src/wrapper.ts
@@ -1,20 +1,62 @@
 import http from "http";
 
+class Wrapper {
 
-http.get("http://localhost:3000/test", resp => {
-  let data: string = "";
+  get(url: string): any {
+    let request = http.get(url, resp => {
+      let data: string = "";
 
-  //A chunk of data has been recived.
-  resp.on("data", chunk => {
-    data += chunk;
-  });
+      if(!this.checkStatus(resp.statusCode)) {
+        console.log(resp.statusMessage);
+        return resp.statusMessage;
+      }
+    
+      resp.on("data", chunk => {
+        data += chunk;
+      });
+    
+      resp.on("end", () => {
+        
+        if(!this.checkContentType(resp.headers)) {
+          console.log("Incorrect header!");
+          return false;
+        }
 
-  //the whole response has been recived. Print out the result.
-  resp.on("end", () => {
-    let url = JSON.parse(data).title;
-    console.log(url);
-  });
-})
-.on("error", err => {
-  console.log("Error: " + err.message);
-});
+        let url = JSON.parse(data).title;
+        console.log(url);
+        return true;
+      });
+
+      return "success";
+
+    })
+    .on("error", err => {
+      console.log("Error: " + err.message);
+    });
+
+    request.end();
+  }
+
+  checkStatus = (statusCode: (number | undefined)): boolean => {
+
+    if(statusCode == 403 || statusCode == 404 || statusCode == 500)
+      return false;
+
+    return true;
+  }
+
+  checkContentType = (headers: any): boolean => {
+  
+    if(headers === undefined)
+      return false;
+
+    let data: string[] = headers["content-type"].split(";");
+
+    if(data[0] == "application/json")
+      return true;
+
+    return false;
+  }
+}
+
+export default Wrapper;
