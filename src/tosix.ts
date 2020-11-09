0c608719cb6b403f932ee20f9994b1dafb4f7d1d
Tomi <developer@livestudio.eu>
2020-11-09 08:23:46 +0100

    add put method

--- src/tosix.ts
+++ src/tosix.ts
@@ -109,6 +109,66 @@ class Tosix {
 		});
 	}
 
+	async put(url: string, rawData: Object): Promise<HttpResponse> {
+		let error: HttpError = {
+			messages: [],
+		};
+		let isError: boolean;
+
+		return new Promise((resolve, reject) => {
+			const options: RequestOptions = {
+				hostname: "localhost",
+				port: 3000,
+				path: url,
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			};
+
+			const request = http.request(options, (res: IncomingMessage) => {
+				if (res.statusCode !== 201 && res.statusCode !== 200) {
+					isError = true;
+					error.messages.push(<string>res.statusMessage);
+				}
+
+				if (!this.checkContentType(res.headers)) {
+					isError = true;
+					error.messages.push("cannot handle the data provided!");
+				}
+
+				let data: ServerResponse;
+				let rawData: string = "";
+				res.on("data", (chunk) => {
+					rawData += chunk.toString("utf8");
+					try {
+						data = JSON.parse(rawData);
+					} catch (ex) {
+						isError = true;
+						error.messages.push("content was not valid json");
+					}
+				});
+
+				res.on("end", () => {
+					if (isError) {
+						reject(error);
+					} else {
+						resolve({
+							statusCode: <number>res.statusCode,
+							data: data.data,
+						});
+					}
+				});
+			});
+
+			request.write(JSON.stringify(rawData), () => {
+				console.log("data has been written!");
+			});
+
+			request.end();
+		});
+	}
+
 	async delete(url: string): Promise<HttpResponse> {
 		let error: HttpError = {
 			messages: [],
